48d3525a79a9ccaafea195a36c5528de15a79ba7
jportella93 <jportella93@gmail.com>
2020-01-31 20:15:18 +0100

    feat: initial commit
    feat: initial commit
    
    reset
    
    feat: initial commit

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
-import './app-banner.js';
-import './dog-card.js';
 import { getDogCollection } from './dogApi.js';
 
 function renderDogs(dogCollection) {
   Object.values(dogCollection).forEach(dogData => {
-    const dogCard = document.createElement('dog-card')
-    dogCard.data = dogData;
+    const dogCard = document.createElement('h2')
+    dogCard.textContent = dogData.breed
     const dogsWrapper = document.querySelector('#dogsWrapper');
     dogsWrapper.appendChild(dogCard)
   });
